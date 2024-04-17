8dc9bf8b7e5a0e7bbcfb6313af8b65d1321f4d71
Jayanta <jbleelija@gmail.com>
2024-04-17 17:55:54 +0530

    modify and create model in  models.py

--- landing/models.py
+++ landing/models.py
@@ -1,19 +1,20 @@
 from django.db import models
 
 # Own models here.
-# class Plans(models.Model):
-#     id = models.IntegerField(primary_key=True)
-#     name = models.CharField(max_length=55)
-#     price = models.FloatField()
-#     duration = models.CharField(max_length=20)
-#     status = models.IntegerField()
+class Plans(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=55)
+    price = models.FloatField()
+    duration = models.CharField(max_length=20)
+    status = models.IntegerField()
 
-#     class Meta:
-#         managed = False
-#         db_table = 'plans'
+    class Meta:
+        managed = False
+        db_table = 'plans'
 
 class PlansFeatures(models.Model):
-    plan_id  = models.IntegerField(primary_key = True)
+    id       = models.IntegerField(primary_key = True)
+    plan_id  = models.IntegerField()
     features = models.CharField(max_length = 100)
     status   = models.IntegerField() 
 
--- landing/views.py
+++ landing/views.py
@@ -1,18 +1,45 @@
 # from django.shortcuts import render
 from django.http import JsonResponse
-# from .models import Plans
+from .models import Plans 
+from .models import  PlansFeatures
 
-# def plans(request):
-#     plans = Plans.objects.all()
+# find plan #
+def plans(request):
+    plans = Plans.objects.all()
 
-#     plan_data = []
-#     for plan in plans:
-#         plan_data.append({
-#             'id': plan.id,
-#             'name': plan.name,
-#             'price': plan.price,
-#             'duration': plan.duration,
-#             'status': plan.status
-#         })
+    plan_data = []
+    for plan in plans:
+        plan_features = PlansFeatures.objects.filter(plan_id=plan.id)
+        features_d = []
+        for plan_fet in plan_features:
+            features_d.append({
+                # 'id': plan_fet.id,
+                'feature': plan_fet.features,
+                'status': plan_fet.status
+            })
+        plan_data.append({
+            'id': plan.id,
+            'name': plan.name,
+            'price': plan.price,
+            'duration': plan.duration,
+            'status': plan.status,
+            'features':features_d
+        })
 
-#     return JsonResponse({'plans': plan_data})  
+    return JsonResponse({'plans': plan_data})  
+
+# find the features #
+def plans_features(request):
+    features = PlansFeatures.objects.all()
+    
+    features_data=[]
+    for plan_fet in features:
+        features_data.append({
+            'id'       :plan_fet.id,
+            'plan_id'  :plan_fet.plan_id,
+            'features' :plan_fet.features,
+            'status'   :plan_fet.status,
+        })
+
+    return JsonResponse({'features': features_data})     
+         
