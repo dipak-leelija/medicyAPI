ba9a28ed20b9fd3bd0da4c8846b5f3082f5281c8
Jayanta <jbleelija@gmail.com>
2024-04-17 14:06:18 +0530

    create new model

--- landing/views.py
+++ landing/views.py
@@ -1,18 +1,18 @@
 # from django.shortcuts import render
 from django.http import JsonResponse
-from .models import Plans
+# from .models import Plans
 
-def plans(request):
-    plans = Plans.objects.all()
+# def plans(request):
+#     plans = Plans.objects.all()
 
-    plan_data = []
-    for plan in plans:
-        plan_data.append({
-            'id': plan.id,
-            'name': plan.name,
-            'price': plan.price,
-            'duration': plan.duration,
-            'status': plan.status
-        })
+#     plan_data = []
+#     for plan in plans:
+#         plan_data.append({
+#             'id': plan.id,
+#             'name': plan.name,
+#             'price': plan.price,
+#             'duration': plan.duration,
+#             'status': plan.status
+#         })
 
-    return JsonResponse({'plans': plan_data})  
+#     return JsonResponse({'plans': plan_data})  
--- medicyLandingAPI/urls.py
+++ medicyLandingAPI/urls.py
@@ -1,9 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path
-from landing.views import plans  
+# from landing.views import plans  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('plans/', plans, name='plans'),
+    # path('plans/', plans, name='plans'),
 ]
